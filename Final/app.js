d460ef95053ea2c64f1c1ccb6dd1dc4244ab3a0a
Nguyen Minh Tien <tiennmak23@cvp.vn>
2023-01-07 15:33:50 +0700

    addfile

--- Final/app.js
+++ Final/app.js
@@ -1,3 +1,50 @@
+let shopping_cart = document.getElementById("shopping-cart");
+
+function abc() {
+  let sum = 0;
+  shopping_cart.innerHTML = "";
+  let data = localStorage.getItem("data");
+  let giohang = JSON.parse(data);
+  let sp = giohang.product;
+  for (let i = 0; i < sp.length; i++) {
+    let st = sp[i].name;
+    let type = "",
+      num = 0;
+    for (let index = 0; index < st.length; index++) {
+      if (st[index] >= "0" && st[index] <= "9")
+        num = num * 10 + st[index] - "0";
+      else type += st[index];
+    }
+    let box = document.createElement("div");
+    box.setAttribute("class", "box");
+    sum += sanpham[type][num].price * sp[i].sl;
+    box.innerHTML = `<i class="fas fa-trash"></i>
+<img src=${sanpham[type][num].urlimg} alt="" />
+<div class="content">
+  <h3>${sanpham[type][num].name}</h3>
+  <span class="price">${sanpham[type][num].price}/-</span>
+  <span class="quantity">sl : ${sp[i].sl}</span>
+</div>`;
+    shopping_cart.append(box);
+  }
+  let total = document.createElement("div");
+  total.setAttribute("class", "total");
+  total.innerHTML = `Tổng số tiền: ${sum}`;
+  shopping_cart.append(total);
+  let a = document.createElement("a");
+  a.setAttribute("class", "btn");
+  a.innerHTML = "Thanh toán";
+  shopping_cart.append(a);
+}
+const UrlApi = "https://62d4116c5112e98e484a08f4.mockapi.io/api/User";
+const updateById = async (id, newData) => {
+  const res = await fetch(UrlApi + `/${id}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(newData),
+  });
+  // console.log(await res.json());
+};
 let searchForm = document.querySelector(".search-form");
 
 document.querySelector("#search-btn").onclick = () => {
@@ -40,42 +87,64 @@ window.onscroll = () => {
   loginForm.classList.remove("active");
   navbar.classList.remove("active");
 };
-let trangtri = [
-  {
-    name: "Chuông",
-    urlimg:
-      "http://media.doisongphapluat.com/297/2014/11/23/mon-do-trang-tri-ngay-giang-sinh-2.jpg",
-    price: 10000,
-  },
-  {
-    name: "Chuông1",
-    urlimg:
-      "http://media.doisongphapluat.com/297/2014/11/23/mon-do-trang-tri-ngay-giang-sinh-2.jpg",
-    price: 10000,
-  },
-  {
-    name: "Chuông2",
-    urlimg:
-      "http://media.doisongphapluat.com/297/2014/11/23/mon-do-trang-tri-ngay-giang-sinh-2.jpg",
-    price: 10000,
-  },
-  {
-    name: "Chuông3",
-    urlimg:
-      "http://media.doisongphapluat.com/297/2014/11/23/mon-do-trang-tri-ngay-giang-sinh-2.jpg",
-    price: 10000,
-  },
-];
+let sanpham = {
+  trangtri: [
+    {
+      name: "Chuông",
+      urlimg:
+        "http://media.doisongphapluat.com/297/2014/11/23/mon-do-trang-tri-ngay-giang-sinh-2.jpg",
+      price: 10000,
+    },
+    {
+      name: "Chuông1",
+      urlimg:
+        "http://media.doisongphapluat.com/297/2014/11/23/mon-do-trang-tri-ngay-giang-sinh-2.jpg",
+      price: 10000,
+    },
+    {
+      name: "Chuông2",
+      urlimg:
+        "http://media.doisongphapluat.com/297/2014/11/23/mon-do-trang-tri-ngay-giang-sinh-2.jpg",
+      price: 10000,
+    },
+    {
+      name: "Chuông3",
+      urlimg:
+        "http://media.doisongphapluat.com/297/2014/11/23/mon-do-trang-tri-ngay-giang-sinh-2.jpg",
+      price: 10000,
+    },
+  ],
+  thucan: [
+    {
+      name: "Đồ ăn 1",
+      urlimg:
+        "http://media.doisongphapluat.com/297/2014/11/23/mon-do-trang-tri-ngay-giang-sinh-2.jpg",
+      price: 10000,
+    },
+    {
+      name: "Đồ ăn 2",
+      urlimg:
+        "http://media.doisongphapluat.com/297/2014/11/23/mon-do-trang-tri-ngay-giang-sinh-2.jpg",
+      price: 10000,
+    },
+    {
+      name: "Đồ ăn 3",
+      urlimg:
+        "http://media.doisongphapluat.com/297/2014/11/23/mon-do-trang-tri-ngay-giang-sinh-2.jpg",
+      price: 10000,
+    },
+  ],
+};
 let trangtri_ele = document.getElementById("trangtri");
-for (let i = 0; i < trangtri.length; i++) {
+for (let i = 0; i < sanpham.trangtri.length; i++) {
   let box = document.createElement("div");
   box.setAttribute("class", "swiper-slide box");
   box.innerHTML = `<img
-src=${trangtri[i].urlimg}
+src=${sanpham.trangtri[i].urlimg}
 alt=""
 />
-<h3>${trangtri[i].name} </h3>
-<div class="price">${trangtri[i].price}</div>
+<h3>${sanpham.trangtri[i].name} </h3>
+<div class="price">${sanpham.trangtri[i].price}</div>
 <div class="stars">
 <i class="fas fa-star"></i>
 <i class="fas fa-star"></i>
@@ -89,9 +158,78 @@ alt=""
   a.innerHTML = "Add";
   box.append(a);
   a.addEventListener("click", (e) => {
-    console.log(e.target.id);
+    // console.log(e.target.id);
+    let st = "trangtri" + String(e.target.id);
+    let data = localStorage.getItem("data");
+    let giohang = JSON.parse(data);
+    let sp = giohang.product;
+    let ok = false;
+    console.log(st);
+    for (let i = 0; i < sp.length; i++) {
+      if (sp[i].name == st) {
+        sp[i].sl++;
+        ok = true;
+      }
+    }
+    if (!ok) {
+      let new_sp = { name: st, sl: 1 };
+      sp.push(new_sp);
+    }
+    // giohang.product.push(st);
+    // giohang.product.splice(0, giohang.product.length);
+    localStorage.setItem("data", JSON.stringify(giohang));
+    abc();
+    updateById(localStorage.id, giohang);
   });
   trangtri_ele.append(box);
+}
+let thucan_ele = document.getElementById("thucan");
+for (let i = 0; i < sanpham.thucan.length; i++) {
+  let box = document.createElement("div");
+  box.setAttribute("class", "swiper-slide box");
+  box.innerHTML = `<img
+src=${sanpham.thucan[i].urlimg}
+alt=""
+/>
+<h3>${sanpham.thucan[i].name} </h3>
+<div class="price">${sanpham.thucan[i].price}</div>
+<div class="stars">
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star"></i>
+<i class="fas fa-star-half-alt"></i>
+</div>`;
+  let a = document.createElement("a");
+  a.setAttribute("class", "btn");
+  a.setAttribute("id", String(i));
+  a.innerHTML = "Add";
+  box.append(a);
+  a.addEventListener("click", (e) => {
+    // console.log(e.target.id);
+    let st = "thucan" + String(e.target.id);
+    let data = localStorage.getItem("data");
+    let giohang = JSON.parse(data);
+    let sp = giohang.product;
+    let ok = false;
+    console.log(st);
+    for (let i = 0; i < sp.length; i++) {
+      if (sp[i].name == st) {
+        sp[i].sl++;
+        ok = true;
+      }
+    }
+    if (!ok) {
+      let new_sp = { name: st, sl: 1 };
+      sp.push(new_sp);
+    }
+    // giohang.product.push(st);
+    // giohang.product.splice(0, giohang.product.length);
+    localStorage.setItem("data", JSON.stringify(giohang));
+    updateById(localStorage.id, giohang);
+    abc();
+  });
+  thucan_ele.append(box);
 }
 var swiper = new Swiper(".product-slider", {
   loop: true,
@@ -134,3 +272,7 @@ var swiper = new Swiper(".review-slider", {
     },
   },
 });
+
+let username_area = document.getElementById("username_area");
+username_area.innerHTML = localStorage.username;
+abc();
